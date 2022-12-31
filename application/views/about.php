add3957b3d55635194da213437d52f3ed5c05e7f
mzahwanlatif <mzahwanlatif@student.telkomuniversity.ac.id>
2022-12-31 10:27:01 +0700

    about.php

--- application/views/about.php
+++ application/views/about.php
@@ -16,6 +16,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     .container{
     background-image: linear-gradient(rgba(0,0,0,0.3),transparent),url('<?php echo base_url("assets/images/background1.png");?>');
     }
+    .row{
+    box-sizing: border-box;
+
+    position: absolute;
+    width: 850px;
+    height: 388px;
+    left: 494px;
+    top: 230px; 
+
+    background: #000000;
+    opacity: 0.5;
+    border: 1px solid #FFFFFF;
+    border-radius: 50px;
+    }
+    
+    .about_me{
+    box-sizing: border-box;
+
+    position: absolute;
+    top: 250px;
+    width: 237px;
+    height: 388px;
+
+    opacity: 0.95;
+    border: 2px solid #484747;
+    border-radius: 50px;
+    
+    }
+    .row , .about_me{
+        z-index: 1;
+    }
     </style>
 </head>
 <body>
@@ -33,21 +64,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <img src="assets/images/menu-btn.png" alt="menu hamburger" class="menu-hamburger">
 
         </nav>
-        <div class="d-flex flex-row">
-            <img src="assets/images/aboutme.png" class="rounded-3 about_me" alt="" width="237px" height="388px">
-            
-                <ul class="row " >
-                    <a>Tokyo</a>
-                    <p>Tokyo adalah ibu kota dan kota terbesar di Jepang. Terletak di pulau Honshu di bagian 
-                    tenggara negara tersebut, Tokyo merupakan pusat ekonomi, politik, dan budaya Jepang. 
-                    Kota ini terkenal dengan budayanya yang unik dan menarik, serta kemajuan teknologinya 
-                    yang tinggi. Tokyo juga memiliki beberapa tempat wisata populer seperti Tokyo Skytree, 
-                    Senso-ji Temple, dan Tokyo Disneyland. Selain itu, kota ini juga merupakan tempat yang 
-                    tepat untuk menikmati makanan Jepang yang lezat, seperti sushi, ramen, dan tempura.</p>
-                </ul>
-            
-        </div>
         
+        <img src="assets/images/aboutme.png" class="about_me">
+        <ul class="row" >
+            <a>Tokyo</a>
+            <p>Tokyo adalah ibu kota dan kota terbesar di Jepang. Terletak di pulau Honshu di bagian 
+            tenggara negara tersebut, Tokyo merupakan pusat ekonomi, politik, dan budaya Jepang. 
+            Kota ini terkenal dengan budayanya yang unik dan menarik, serta kemajuan teknologinya 
+            yang tinggi. Tokyo juga memiliki beberapa tempat wisata populer seperti Tokyo Skytree, 
+            Senso-ji Temple, dan Tokyo Disneyland. Selain itu, kota ini juga merupakan tempat yang 
+            tepat untuk menikmati makanan Jepang yang lezat, seperti sushi, ramen, dan tempura.</p>
+        </ul>
+
     </div>
 </body>
     <script>
